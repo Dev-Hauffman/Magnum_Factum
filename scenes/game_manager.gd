186288e48ddb1e3b90768fb54b071933c76cc751
Dev-Hauffman <rauf.rodrigues@acad.pucrs.br>
2023-09-09 19:42:58 -0300

    reworked game_manager

--- scenes/game_manager.gd
+++ scenes/game_manager.gd
@@ -1,48 +1,29 @@
 extends Node
 
 
-var choose_semester_scene
-
 var available_disciplines:Array = []
 var current_disciplines:Dictionary = {}
 var current_semester:int = 1
 
 
 func _ready():
-	available_disciplines = Disciplines.get_existing_disciplines()
-	add_choose_semester_scene()
-
-
-func add_choose_semester_scene():
-	choose_semester_scene = preload("res://scenes/choose_semester/choose_semester_screen.tscn").instantiate()
-	add_child(choose_semester_scene)
-	choose_semester_scene.initialize(available_disciplines)
-	choose_semester_scene.connect("disciplines_chosen", Callable(self, "treat_chosen_disciplines"))
-
-
-func treat_chosen_disciplines(remaining_diciplines:Array, chosen_disciplines:Array):
-	available_disciplines = remaining_diciplines
-	for discipline in chosen_disciplines:
-		var content:Dictionary = {}
-		content["discipline_name"] = discipline[0]
-		content["level"] = discipline[1]
-		current_disciplines[discipline[0]] = content
+	available_disciplines = Disciplines.get_discipline_by_semester(current_semester)
+	# play_show_semester_scene() #shows current disciplines and semester in a scene
+	
 	create_tests()
 
 
 func create_tests():
-	for discipline in current_disciplines:
+	for discipline in available_disciplines:
 		var tests:Dictionary = {}
-		var string = "test"
+		var test_string = "test"
 		var counter = 1
-		#var number_of_tests = randi_range(2,3)
-		var number_of_tests = 2
+		var number_of_tests = 2 # in the future might be 3 to 2 tests without G2
 		tests["quantity"] = number_of_tests
 		var scores:Dictionary = {}
 		for score in range(number_of_tests):
-			scores[string + str(counter)] = -1
+			scores[test_string + str(counter)] = -1
 			counter += 1
 		counter = 1
 		tests["scores"] = scores
-		current_disciplines[discipline]["tests"] = tests
-		
+		current_disciplines[discipline] = tests
