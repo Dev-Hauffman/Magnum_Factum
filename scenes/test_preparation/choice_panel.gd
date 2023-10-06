2c4ec8bbe0769d95a44ec9e814e4ba6bbb4d990a
Dev-Hauffman <rauf.rodrigues@acad.pucrs.br>
2023-10-06 08:09:42 -0300

    progress bar flash

--- scenes/test_preparation/choice_panel.gd
+++ scenes/test_preparation/choice_panel.gd
@@ -4,6 +4,10 @@ class_name ChoiceContainer
 
 signal study_selected
 signal rest_selected
+signal study_mouse_entered
+signal rest_mouse_entered
+signal study_mouse_left
+signal rest_mouse_left
 
 
 @onready var week_label:Label = $MarginContainer/VBoxContainer/WeekLabel
@@ -27,3 +31,23 @@ func _on_rest_button_pressed():
 	emit_signal("rest_selected")
 	rest_button.disabled = true
 	study_button.disabled = true
+
+
+func _on_study_button_mouse_entered():
+	if not study_button.disabled:
+		emit_signal("study_mouse_entered")
+
+
+func _on_study_button_mouse_exited():
+	if not study_button.disabled:
+		emit_signal("study_mouse_left")
+
+
+func _on_rest_button_mouse_entered():
+	if not study_button.disabled:
+		emit_signal("rest_mouse_entered")
+
+
+func _on_rest_button_mouse_exited():
+	if not study_button.disabled:
+		emit_signal("rest_mouse_left")
--- scenes/test_preparation/test_preparation.gd
+++ scenes/test_preparation/test_preparation.gd
@@ -8,7 +8,7 @@ signal stress_defined(stress_amount)
 var number_of_weeks:int = 4
 var rest_amount:int = 15
 var stress_amount:int = 25
-var writting_speed:int = 10
+var writing_speed:int = 10
 var precision:int
 var choice_panels:Array = []
 var choice_panels_index:int = 1
@@ -19,7 +19,12 @@ var choice_panels_index:int = 1
 @onready var tiredness_bar:ProgressBar = $HBoxContainer3/VBoxContainer2/TirednessContainer/TirednessBar
 @onready var knowledge_bar:ProgressBar = $HBoxContainer3/VBoxContainer2/KnowledgeContainer/KnowledgeBar
 @onready var test_start_button = $VBoxContainer/ChoiceContainer/TestContainer/MarginContainer/VBoxContainer/TestStartButton
-
+@onready var writing_speed_value = $HBoxContainer3/VBoxContainer2/TirednessContainer/TirednessBar/WritingSpeedValue
+@onready var answer_precision_value = $HBoxContainer3/VBoxContainer2/KnowledgeContainer/KnowledgeBar/AnswerPrecisionValue
+@onready var tiredness_bar_preview = $HBoxContainer3/VBoxContainer2/TirednessContainer/TirednessBar/TirednessBarPreview
+@onready var knowledge_bar_preview = $HBoxContainer3/VBoxContainer2/KnowledgeContainer/KnowledgeBar/KnowledgeBarPreview
+@onready var tiredness_preview_animation = $HBoxContainer3/VBoxContainer2/TirednessContainer/TirednessBar/TirednessBarPreview/TirednessPreviewAnimation
+@onready var knowledge_animation_preview = $HBoxContainer3/VBoxContainer2/KnowledgeContainer/KnowledgeBar/KnowledgeBarPreview/KnowledgeAnimationPreview
 
 
 func initialize(test_name:String, test_number:String, stress_level:int, semester_number:int):
@@ -39,6 +44,10 @@ func populate_choices():
 		choice_container.move_child(choice_panel, counter - 1)
 		choice_panel.connect("study_selected", Callable(self, "study_selected"))
 		choice_panel.connect("rest_selected", Callable(self, "rest_selected"))
+		choice_panel.connect("study_mouse_entered", Callable(self, "add_study_preview"))
+		choice_panel.connect("study_mouse_left", Callable(self, "remove_preview"))
+		choice_panel.connect("rest_mouse_entered", Callable(self, "add_rest_preview"))
+		choice_panel.connect("rest_mouse_left", Callable(self, "remove_preview"))
 		choice_panels.append(choice_panel)
 		if counter == 1:
 			choice_panel.study_button.disabled = false
@@ -46,7 +55,38 @@ func populate_choices():
 		counter += 1
 
 
+func add_study_preview():
+	if knowledge_bar.value != 100:
+		knowledge_bar_preview.value = knowledge_bar.value + 100 / number_of_weeks
+	else:
+		knowledge_bar_preview.value = 100
+	if tiredness_bar.value != 100:
+		tiredness_bar_preview.value = tiredness_bar.value + stress_amount
+	else:
+		tiredness_bar_preview.value = 100
+	knowledge_animation_preview.play("Pulse")
+	tiredness_preview_animation.play("Pulse")
+
+
+func remove_preview():
+	knowledge_bar_preview.value = 0
+	tiredness_bar_preview.value = 0
+	if tiredness_preview_animation.is_playing():
+		tiredness_preview_animation.stop()
+	if knowledge_animation_preview.is_playing():
+		knowledge_animation_preview.stop()
+
+
+func add_rest_preview():
+	if tiredness_bar.value != 0:
+		tiredness_bar_preview.value = tiredness_bar.value - rest_amount
+		tiredness_preview_animation.play("Pulse")
+	else:
+		tiredness_bar_preview.value = 0
+
+
 func study_selected():
+	remove_preview()
 	knowledge_bar.value += 100 / number_of_weeks
 	if tiredness_bar.value < 100: 
 		tiredness_bar.value += stress_amount
@@ -67,9 +107,11 @@ func update_precision_value():
 	else:
 		precision = 20
 	knowledge_bar.tooltip_text = "Your answer precision is " + str(precision)
+	answer_precision_value.text = "Answer Precision: " + str(precision) +"%"
 
 
 func rest_selected():
+	remove_preview()
 	if tiredness_bar.value - rest_amount < 0:
 		tiredness_bar.value = 0
 	else:
@@ -80,10 +122,11 @@ func rest_selected():
 
 
 func update_speed_value():
-	writting_speed = 20 - (tiredness_bar.value/10)
-	if writting_speed == 0:
-		writting_speed = 10
-	tiredness_bar.tooltip_text = "Your writing speed is at " + str(writting_speed)
+	writing_speed = 20 - (tiredness_bar.value/10)
+	if writing_speed == 0:
+		writing_speed = 10
+	tiredness_bar.tooltip_text = "Your writing speed is at " + str(writing_speed)
+	writing_speed_value.text = "Writing Speed:" + str(writing_speed)
 
 
 func enable_next_choice_panel():
@@ -98,5 +141,5 @@ func enable_next_choice_panel():
 
 
 func _on_button_pressed():
-	emit_signal("finished_preparation", writting_speed, precision)
+	emit_signal("finished_preparation", writing_speed, precision)
 	emit_signal("stress_defined", tiredness_bar.value)
